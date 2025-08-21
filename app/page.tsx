b8dbf38231a593153e7ecf54e4ddd2f6733ca7f7
Luka Adzic <lukaadz@wharton.upenn.edu>
2025-08-21 18:34:11 +0200

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ const DescriptionTypewriter = ({ speed = 1 }: { speed?: number }) => {
 		() => [
 			{
 				text: "Replacing your chief of staff with ",
-				links: [{ text: "@\u00A0PennyOne", href: "https://penny-one.com" }],
+				links: [{ text: "@\u00A0Penny-One", href: "https://penny-one.com" }],
 				suffix: ".",
 			},
 			{
